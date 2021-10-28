49762d221708faef7bbced44f28af1986f713acf
Phoebe Wienke <lie@teamwienke.dk>
2021-10-28 14:08:32 +0200

    Functions

--- Bacteria Data Analysis.py
+++ Bacteria Data Analysis.py
@@ -1,1 +1,13 @@
-#new file
+# Bacteria Data Analysis Project
+#
+
+def dataLoad(filename):
+# Insert your code here
+return data
+
+def dataStatistics(data, statistic):
+# Insert your code here
+return result
+
+def dataPlot(data):
+# Insert your code her
