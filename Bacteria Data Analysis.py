0359136db35381c71144ec5055f354a2680005bc
PhoenixPhoebe <line@teamwienke.dk>
2021-11-01 20:57:25 +0100

    dataload funtion er sat op

--- Bacteria Data Analysis.py
+++ Bacteria Data Analysis.py
@@ -1,16 +1,26 @@
 # Bacteria Data Analysis Project
-#
+
+import numpy as np
+from numpy.lib.shape_base import split
+#Todo Error Handling
+
+
 
 def dataLoad(filename):
-# Insert your code here
 
     filein = open(filename,"r")
     lines =filein.readlines()
-    data = "".join(lines)
-    return data
-
+    data = "".join(lines).split() #den ligger alle linjerne sammen i en string, som så bliver splittet op med hver værdier i en liste
+    dataMatrix = []
+    #while loop løber så længe data listen ikke er tom.
+    while data != []:
+        dataMatrix.append(data[:3]) #tager de første tre på listen og ligger i matrixen
+        data = data[3:] #ligger alle på nær de første tre i samme variable (fjerner de første tre)
+    
+    return np.array(dataMatrix)
 
 print(dataLoad("test.txt"))
+#print(dataLoad("test.txt"))
 #def dataStatistics(data, statistic):
 # Insert your code here
 #return result
